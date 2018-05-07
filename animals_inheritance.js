c113f76c7d42663e03d870c3b88bea6a2f9a2028
justinusamwijaya <justin.am.wijaya@gmail.com>
2018-05-07 15:33:03 +0700

    class Inheritance *SUPA POWAAAAAA*

--- animals_inheritance.js
+++ animals_inheritance.js
@@ -1,1 +1,42 @@
 "use strict"
+
+class Animal{
+    constructor(animalProperties){
+        this.legsNumber = animalProperties.legsNumber
+        this.isWarmBlooded = animalProperties.isWarmBlooded
+        this.Name = animalProperties.Name
+        this.SupaPowa = new SupaPowa()
+    }
+
+}
+
+class Godzilla extends Animal{
+    constructor(animalProperties){
+        super(animalProperties)
+        this.hotness = animalProperties.hotness
+    }
+}
+
+class SupaPowa {
+    constructor(){
+        
+    }
+    useLaserVision(){
+        console.log('lalalallalallalalalala')
+    }
+    flyLikeARetard(){
+        console.log('bewewealjalsfhdalds')
+    }
+    beSupaHot(){
+        console.log('OOOOOOOH YEAAAAAAAAAH')
+    }
+}
+
+let Bob = new Godzilla({
+    Name : 'Bob',
+    legsNumber : 500,
+    isWarmBlooded : 'OF COURSE',
+    hotness : 'SUPA HOT',
+})
+
+Bob.SupaPowa.beSupaHot()
